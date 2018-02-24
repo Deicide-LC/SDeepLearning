145e065d359c8330c075c2990d39854f24d1f2a6
taegoon <hantaegune@gmail.com>
2018-02-24 16:13:29 +0900

    first commit

--- DL_02_01_linear_regression.py
+++ DL_02_01_linear_regression.py
@@ -12,6 +12,7 @@ b = tf.Variable(tf.random_normal([1]), name = 'bias')
 
 hypothesis = x_train * W + b
 
+
 cost = tf.reduce_mean(tf.square(hypothesis - y_train))
 
 optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
